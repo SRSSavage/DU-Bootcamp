a2d386b6c65209138297936b06a92b1e3857223f
SRSSavage <zach2487@gmail.com>
2019-06-20 18:56:12 -0600

    added modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,9 +3,7 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
+
 
   def create
     @quote = Quote.create(quote_params)
@@ -15,6 +13,8 @@ class QuotesController < ApplicationController
     redirect_to root_path
   end
 
+  def about
+  end
   private
 
   def quote_params
